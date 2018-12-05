47ede76a948b2336f4373eff4b71aa2ef69c810e
Keith Hatfield <keith.hatfield@gmail.com>
2018-12-05 14:03:40 -0500

    Fix WSDL path

--- src/config/config.php
+++ src/config/config.php
@@ -5,5 +5,5 @@ return [
     'password'        => env('SALESFORCE_PASSWORD'),
     'token'           => env('SALESFORCE_TOKEN'),
     'connection_type' => env('SALESFORCE_CONNECTION_TYPE'),
-    'wsdl'            => storage_path('app/' . env('SALESFORCE_WSDL', 'wsdl/enterprise.wsdl.xml')),
+    'wsdl'            => storage_path('app/wsdl/' . env('SALESFORCE_WSDL', 'enterprise.wsdl.xml')),
 ];
