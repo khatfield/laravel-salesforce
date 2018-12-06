0ca37dbd330143fafc061878b64272a613927e37
Keith Hatfield <keith.hatfield@gmail.com>
2018-12-06 14:48:39 -0500

    Move request macros to new service provider

---
+++ src/Providers/SalesforceRequestServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace Khatfield\LaravelSalesforce\Providers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\ServiceProvider;
+
+class SalesforceRequestServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //register request macro
+        Request::macro('isSalesforce', function()
+        {
+            $is_xml = (strtolower($this->getContentType()) == 'xml');
+
+            return ($is_xml && stripos($this->getContent(), 'sforce'));
+        });
+
+        Request::macro('salesforce', function()
+        {
+            $return  = new SObject();
+            $content = $this->getContent();
+
+            if($this->isSalesforce() && !empty($content)) {
+                $sobject = null;
+
+                try {
+                    $xml     = simplexml_load_string($content);
+                    $body    = $xml->children('http://schemas.xmlsoap.org/soap/envelope/')
+                        ->Body->children('http://soap.sforce.com/2005/09/outbound');
+                    $sobject = $body->notifications->Notification->sObject->children('urn:sobject.enterprise.soap.sforce.com');
+
+                } catch(\Exception $e) {
+                    $error = new \Khatfield\LaravelSalesforce\Exceptions\SalesforceException('Error getting Salesforce POST: ' . $e->getMessage(), $e->getCode(), $e);
+                    report($error);
+                    $sobject = false;
+                }
+
+                if(is_object($sobject)) {
+                    foreach($sobject as $field => $value) {
+                        $return->$field = $value;
+                    }
+                }
+            }
+
+            return $return;
+        });
+    }
+}
